f71b0d57e5234c8884c0285404eb660b1ac30c46
MaedaKouta <maedakoutaa@gmail.com>
2022-02-20 08:17:21 +0900

    commit

--- MiniApp10-TableViewCellAdd/ViewController.swift
+++ MiniApp10-TableViewCellAdd/ViewController.swift
@@ -7,13 +7,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet private weak var textField: UITextField!
+    @IBOutlet private weak var tableView: UITableView!
+    private var texts: [String] = []
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return texts.count
     }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell1")!
+        cell.textLabel?.text = texts[indexPath.row]
+        return cell
+    }
 
+    @IBAction func didTapAddButton(_ sender: Any) {
+        if textField?.text?.isEmpty == false {
+            texts.append(textField.text!)
+            textField.text = ""
+            tableView.reloadData()
+        }
+    }
+
+    @IBAction func didTapRemoveButton(_ sender: Any) {
+        if (0 < texts.count) {
+            texts.removeLast()
+            tableView.reloadData()
+        }
+    }
 }
-
